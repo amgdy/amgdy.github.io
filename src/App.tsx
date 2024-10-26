999d50a4e3bec86d22fde955704629b3f25736b9
Ahmed Magdy <amagdy@microsoft.com>
2024-10-26 18:36:01 +0300

    Refactor deployment workflow and update Node version
    - Refactored the deployment workflow in the `.github/workflows/deploy.yml` file to improve readability and maintainability.
    - Updated the Node version in the workflow to use "20.x" instead of just "20" to ensure compatibility with the latest Node releases.
    - Changed the `npm install` command to `npm ci` for faster and more reliable dependency installation.
    
    Fix CSS styling and add page background color
    
    - Removed unnecessary CSS rules from the `App.css` file.
    - Added a background color to the `body` element in the `index.css` file to improve the visual appearance of the website.
    
    Set document title in App component
    
    - Added an `useEffect` hook in the `App.tsx` file to set the document title to "Ahmed Magdy Personal Website" when the component mounts.
    
    Add launch configuration for debugging
    
    - Added a `launch.json` file in the `.vscode` directory to configure the launch behavior for debugging the application using the command `npm start`.
    
    Signed-off-by: Ahmed Magdy <amagdy@microsoft.com>

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,12 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import './App.css';
 import { NavBar } from './components/nav-bar/nav-bar';
 import { Greeting } from './components/greetings/greetings';
 
 function App() {
+  useEffect(() => {
+    document.title = "Ahmed Magdy Personal Website";
+  }, []);
   return (
     <div className="App">
       <div>
